875b1e137df00b6eae45cb4b7dc412010fafca69
Madhan Kumar K <madhan.k.gym@gmail.com>
2017-06-25 20:54:29 +0530

    coppied jenkinTestWithGit project to git location

---
+++ jenkinTestWithGit/src/main/java/jenkinTestWithGit/TestJava.java
@@ -0,0 +1,11 @@
+package jenkinTestWithGit;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test Completed");
+	}
+
+}
