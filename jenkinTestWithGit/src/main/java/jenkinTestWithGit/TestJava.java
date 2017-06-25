fb7624a405853a6c8b464b9be380159fa13ea107
Madhan Kumar K <madhan.k.gym@gmail.com>
2017-06-25 23:24:23 +0530

    test changed
    print statement is changed from "test completed " to "test is completed"

--- jenkinTestWithGit/src/main/java/jenkinTestWithGit/TestJava.java
+++ jenkinTestWithGit/src/main/java/jenkinTestWithGit/TestJava.java
@@ -5,7 +5,7 @@ public class TestJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Test Completed");
+		System.out.println("Test is Completed");
 	}
 
 }
